de11328943ea390cccec36c1c6aa916f9ffe4d99
Leo <leonardo.moreno@gmail.com>
2017-07-19 10:34:59 -0500

    Initial commit

---
+++ src/main/java/net/velocity/test/services/PrimeService.java
@@ -0,0 +1,17 @@
+package net.velocity.test.services;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PrimeService {
+	
+	/**
+	 * Check if parameter is a natural number greater than 1 that has no positive divisors other than 1 and itself
+	 * @param number
+	 * @return
+	 */
+	public boolean isPrime(int number){
+		return false;
+	}
+
+}
